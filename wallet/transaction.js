b93f80bc2ed3be5f62bbd21ebd898873e931da99
Shattek-T <xt3750@rit.edu>
2025-05-31 22:29:51 -0400

    Update transaction.js

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,5 +1,6 @@
 // Copyright (c) 2025 Shattek-T. All rights reserved.
 const ChainUtil = require('../chain-util');
+const { MINING_REWARD } = require('../config');
 
 class Transaction {
     constructor() {
@@ -23,21 +24,39 @@ class Transaction {
         return this;
     }
 
-    static newTransaction(senderWallet, recipient, amount) {
+    static transactionWithOutputs(senderWallet, outputs){
         const transaction = new this();
+        transaction.outputs.push(...outputs);
+        Transaction.signTransaction(transaction, senderWallet);
+        return transaction;
+    }
+
+    static newTransaction(senderWallet, recipient, amount) {
+        // const transaction = new this();
 
         if(amount > senderWallet.balance){
             console.log(`Amount: ${amount} exceeds balance.`);
             return;
         }
 
-        transaction.outputs.push(...[
+        // transaction.outputs.push(...[
+        //     { amount: senderWallet.balance - amount, address: senderWallet.publicKey },
+        //     { amount, address: recipient }
+        // ])
+        // Transaction.signTransaction(transaction, senderWallet);
+
+        return Transaction.transactionWithOutputs(senderWallet, [
             { amount: senderWallet.balance - amount, address: senderWallet.publicKey },
             { amount, address: recipient }
-        ])
-        Transaction.signTransaction(transaction, senderWallet);
+        ]);
 
-        return transaction;
+        // return transaction;
+    }
+
+    static rewardTransaction(minerWallet, blockchainWallet) {
+        return Transaction.transactionWithOutputs(blockchainWallet, [{
+            amount: MINING_REWARD, address: minerWallet.publicKey
+        }]);
     }
 
     static signTransaction(transaction, senderWallet){
