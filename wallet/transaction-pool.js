0d6af7d8b896bf89760c2489324ece0d6eff1d3a
Shattek-T <xt3750@rit.edu>
2025-05-31 22:30:27 -0400

    Update transaction-pool.js

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -1,4 +1,5 @@
 // Copyright (c) 2025 Shattek-T. All rights reserved.
+const Transaction = require('../wallet/transaction');
 class TransactionPool {
     constructor() {
         this.transactions = [];
@@ -14,8 +15,32 @@ class TransactionPool {
         }
     }
 
-    existingTransaction(address){
+    existingTransaction(address) {
         return this.transactions.find(t => t.input.address === address);
+    }
+
+    validTransactions() {
+        return this.transactions.filter(transaction => {
+            const outputTotal = transaction.outputs.reduce((total, output) => {
+                return total + output.amount;
+            }, 0);
+
+            if (transaction.input.amount !== outputTotal){
+                console.log(`Invalid transaction from ${transaction.input.address}.`);
+                return;
+            }
+
+            if (!Transaction.verifyTransaction(transaction)){
+                console.log(`Invalid signature from ${transaction.input.address}.`);
+                return;
+            }
+
+            return transaction;
+        });
+    }
+
+    clear() {
+        this.transactions = [];
     }
 }
 
